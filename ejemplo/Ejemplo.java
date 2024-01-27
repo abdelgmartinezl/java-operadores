c280aa26fab4521964d3a50b264eea4692f044e7
Abdel Gadiel Martinez Lassonde <abdel.g.martinez.l@gmail.com>
2024-01-27 15:57:00 -0500

    Inclusión de los operadores aritmeticos comunes.

---
+++ ejemplo/Ejemplo.java
@@ -0,0 +1,21 @@
+package ejemplo;
+
+public class Ejemplo {
+    public static void main (String[] args){
+        // Operadores aritmeticos
+        int a = 20, b = 5;
+        int suma = a + b;
+        int resta = a - b;
+        int multiplicacion = a * b;
+        int division = a / b;
+        int residuo = a % b;
+
+        System.out.println("Operadores Aritmeticos: ");
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicacion: " + multiplicacion);
+        System.out.println("Division: " + division);
+        System.out.println("Residuo: " + residuo);
+
+    }
+}
